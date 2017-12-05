1d572895d33e9680c9b3406e3d4aa418b03181fe
fabed2976 <fabed2976@HRH-IC0027296.wrdsb.ca>
2017-12-05 12:28:45 -0500

    not finished but getting there

--- src/Location.java
+++ src/Location.java
@@ -1,20 +1,52 @@
-
+import java.util.Scanner;
 
 
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author fabed2976
  */
 public class Location {
-    
+    //created instance variables available throughout program
+    //indicates the location name 
+    private String locationName;
+    //stores images as an array from Screen class
+    private Screen[] locationImage;
+    //keeps track of the amount of images in location
+    private int ImageNum;
 
+    //method to identify the image with location
+    public Location(Scanner input) {
+       //scans for the location name
+       locationName = input.nextLine();
+       //be able to retrieve the photos within the location
+        locationImage = new Screen[4];
+        //store the number of images
+        ImageNum = 4;
+        //created a loop that goes through each image
+        for(int i=0;i<ImageNum;i++){
+            //input is received from screen class due to method in scanning the text file 
+            Screen display = new Screen(input);
+            locationImage[i]=display;
         }
-    
-    
-    
+    }
+       
+ 
+    //call location name
+    public String Lname(String locateName) {
+        return this.locationName;
+    }
 
+    //call image of the location
+    public String Limg(int position) {
+        return locationImage[position].getImage();
+    }
+    
+    //call the amount of images within a location
+    public int numOfImgs(int ImageNum) {
+        return this.ImageNum;
+    }
+}
