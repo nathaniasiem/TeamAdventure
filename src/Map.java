4609a19cc97a3e3610842cd977037d151856aa41
fabed2976 <fabed2976@HRH-IC0027296.wrdsb.ca>
2017-12-06 13:05:46 -0500

    Finished controller but I need the Screen, and location

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,8 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
  * To change this template, choose Tools | Templates
@@ -13,15 +16,44 @@ import java.util.ArrayList;
 public class Map {
     
     //create an instance variable for the initial location
-    private String initialLocation;
+    private String curLocation;
     
     //create an instance variable for the initial direction
-    private String initialDirection;
+    private String curDirection;
     
     //create an Array list of the location
     private ArrayList<Location> locations;
     
-    public Map(){
+    public Map(String fileName){
+        //intialize the array list
+        locations = new ArrayList<>();
+        try{
+            
+            //make the scanner for the text File
+            Scanner in = new Scanner(new File(fileName));
+            
+            //initialize the current location as the "word" / position
+            curLocation = in.next();
+            
+            ////initialize the current direction as the "word" / position
+            curDirection = in.next();
+            
+            //Make Screens
+            //loop to the end of the file
+            while(in.hasNext()){
+                
+                //creatinf a screen from the scanner
+                Location scr = new Location(in);
+                
+                //added the created screen to the list
+                locations.add(scr);
+                
+            }
+            //throws the error if file is not found (fileName)
+        }catch(FileNotFoundException ex){
+            ex.printStackTrace();
+            
+        }
         
     }
     
@@ -30,7 +62,7 @@ public class Map {
      * @return the initial location
      */
     public String getIntialLocation(){
-        return initialLocation;
+        return curLocation;
         
     }
     
@@ -39,16 +71,23 @@ public class Map {
      * @return the initial direction
      */
     public String getInitialDirection(){
-        return initialDirection;
+        return curDirection;
     }
     
-    //public get Screen(String findLocation, String findDirection){
-    //for(Location loc: this.locations){
-    //if(loc.getName().equals(findLocation)){
-    //return loc.getScreen(findDirection);
-    //}
-    //}
-    //return null;
+    public Screen getLocation(String loc, String dir){
+        //go through each screen
+        for(Location s: locations){
+            //checks if the name of the locationmatched the locations 
+            if(s.getLocName().equals(loc)){
+                //if it found its macth, asks the method to see if htere is a direction match
+                return s.getScreen(dir);
+                
+            }
+        }
+        //if not matched was found, it will return nothing
+        return null;
+    }
     
+   
 }
 
