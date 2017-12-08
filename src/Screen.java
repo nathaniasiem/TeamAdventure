0685df4a44e53876cf3c3930c437ee1e050eab5f
fabed2976 <fabed2976@HRH-IC0027294.wrdsb.ca>
2017-12-08 13:52:13 -0500

    Finished the program but there is a slight problem on playing the game

--- src/Screen.java
+++ src/Screen.java
@@ -1,4 +1,7 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Scanner;
+import javax.imageio.ImageIO;
 
 /*
  * To change this template, choose Tools | Templates
@@ -10,24 +13,26 @@ import java.util.Scanner;
  * @author fabed2976
  */
 public class Screen {
-    public String image;
+    public BufferedImage image;
     public boolean isBlocked;
     public String currentLocation;
     public String nextLocation;
     public String direction;
     public String nextDirection;
     public String locationName;
+    private String imageName;
+
     
-    //method to scan in the file
+    //method to scan in the file to display image
     public Screen(Scanner input){
         //checks the direction user is facing
         direction = input.next();
         //reads the image name
-        image = input.next();
-        //
-        String check = input.next();
+        imageName = input.next();
+        //reads the indicator if it is true or false
+        String wall = input.next();
         //condition when checking if user is blocked
-        if(check.equals("false")){
+        if(wall.equals("false")){
             isBlocked=false;
             nextLocation = input.next();
             nextDirection = input.next();
@@ -35,18 +40,19 @@ public class Screen {
             isBlocked=true;
             input.next();
         }
+        
+        try{
+            image = ImageIO.read(new File ("pics.txt" + imageName));
+        } catch(Exception e){
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
     //method that returns the location of the user
     public String cLocation(String user){
         return currentLocation;
     }
-    
-    //method that identifies if the user is able to move or not
-    
-    public boolean getblock (){
-        return isBlocked;
-        }   
-    
+ 
     //method that identifies the direction the user is currently facing
     public String getDirection (){
         return direction;
@@ -58,17 +64,20 @@ public class Screen {
     }
     
     //method to give the next location 
-    public String getNextLocation (String nextLocation){
+    public String getNextLocation (){
         return nextLocation;
     }
     
     //method to retrieve the image
-    public String getImage(){
-        return this.image;
+    public BufferedImage getImage(){
+        return image;
+
     }
+    //method that identifies if the user is able to move or not
     
-    //call location name
-    public String Lname() {
-        return this.locationName;
-    }
+    public boolean getblock (String direction){
+        return isBlocked;
+        }   
+
+
 }
